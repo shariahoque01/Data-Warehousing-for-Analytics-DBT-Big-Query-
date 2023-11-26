01e26c36c078050a01338a6e33e2f214200271c4
Sharia Hoque <sharia.hoque@baruchmail.cuny.edu>
2023-11-26 20:41:53 +0000

    comiit

--- models/dimensions/complaint_type.sql
+++ models/dimensions/complaint_type.sql
@@ -1,18 +1,21 @@
-{{
-    config(
-        materialized = 'table'
+{{ config(materialized="table") }}
+
+
+with
+    complaint_type as (
+
+        select distinct (complaint_type), descriptor
+
+        from `cis4400project-403800.projectDatasets.311_service_requests`
+
     )
-}}
 
+select row_number() over (order by (descriptor)) as sks, *
+from
+    complaint_type
 
-with complaint_type as (
-
-    select
-        distinct(Complaint_Type), Descriptor
-
-FROM `cis4400project-403800.projectDatasets.311_traffic_lights`
-
-)
-select row_number() over (order by Descriptor) as SKs,
-* from complaint_type
-
+    -- select
+    -- distinct(Complaint_Type), Descriptor, row_number() over (order by
+    -- distinct(Descriptor)) as SKs
+    -- FROM `cis4400project-403800.projectDatasets.311_traffic_lights`
+    
